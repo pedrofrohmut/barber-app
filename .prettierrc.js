52f0a89671994552d414a60e4d82a6c1e16eae93
pedrofrohmut <pedrofrohmut@gmail.com>
2021-03-09 16:38:41 -0300

    Hello World

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuotes: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  endOfLine: "lf"
+}

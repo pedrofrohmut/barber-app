6e056a983807e77afbf81521eb4a7db2af91a201
pedrofrohmut <pedrofrohmut@gmail.com>
2021-03-10 10:56:41 -0300

    Stack & Screens Initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react"
-import { Text } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+
+import MainStack from "./stacks/MainStack"
 
 const App = () => {
-  return <Text>Hello World!</Text>
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  )
 }
 
 export default App

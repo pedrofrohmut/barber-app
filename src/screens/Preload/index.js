819f148ec24fc38139a257b08f345f42c0c4868e
pedrofrohmut <pedrofrohmut@gmail.com>
2021-03-10 11:46:42 -0300

    Screen Preload

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,12 +1,30 @@
-import React from "react"
-import { Text } from "react-native"
+import React, { useEffect } from "react"
+import AsyncStorage from "@react-native-community/async-storage"
+import { useNavigation } from "@react-navigation/native"
 
-import { Container } from "./styles"
+import BarberLogo from "../../assets/barber.svg"
+
+import { Container, LoadingIcon } from "./styles"
 
 const Preload = () => {
+  const navigation = useNavigation()
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem("token")
+    if (token) {
+    } else {
+      navigation.navigate("SignIn")
+    }
+  }
+
+  useEffect(() => {
+    checkToken()
+  }, [])
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#ffffff" />
     </Container>
   )
 }
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -9,7 +9,12 @@ const Stack = createStackNavigator()
 
 const MainStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Preload"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
       <Stack.Screen name="Preload" component={Preload} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
